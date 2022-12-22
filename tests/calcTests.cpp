42dc14a5db6f519a4eb3b2d9072589c7d30b61a1
kamskikh <79931099+kamskikh@users.noreply.github.com>
2022-12-22 03:48:58 +0300

    Add files via upload

---
+++ tests/calcTests.cpp
@@ -0,0 +1,34 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest.h"
+#include "../calc.h"
+
+
+TEST_CASE("plus test") {
+    SUBCASE("++") CHECK(cplus(1, 1) == 2);
+    SUBCASE("--") CHECK(cplus(-1, -1) == -2);
+    SUBCASE("+-") CHECK(cplus(1, -1) == 0);
+    SUBCASE("-+") CHECK(cplus(-1, 1) == 0);
+    //SUBCASE("broke") CHECK(cbrokePlus(1, 1) == 15);
+}
+
+TEST_CASE("minus test") {
+    SUBCASE("++") CHECK(cminus(1, 1) == 0);
+    SUBCASE("--") CHECK(cminus(-1, -1) == 0);
+    SUBCASE("+-") CHECK(cminus(1, -1) == 2);
+    SUBCASE("-+") CHECK(cminus(-1, 1) == -2);
+}
+
+TEST_CASE("multiply test") {
+    SUBCASE("++") CHECK(cmultiply(2, 2) == 4);
+    SUBCASE("--") CHECK(cmultiply(-2, -2) == 4);
+    SUBCASE("+-") CHECK(cmultiply(2, -2) == -4);
+    SUBCASE("-+") CHECK(cmultiply(-2, 2) == -4);
+}
+
+TEST_CASE("divide test") {
+    SUBCASE("++") CHECK(cdivide(2, 2) == 1);
+    SUBCASE("--") CHECK(cdivide(-2, -2) == 1);
+    SUBCASE("+-") CHECK(cdivide(2, -2) == -1);
+    SUBCASE("-+") CHECK(cdivide(-2, 2) == -1);
+}
